f18b492bfa49fc9aed753d708aecdda8ec594fb8
pseguel27 <pato.seguel.moya@gmail.com>
2017-12-04 18:38:05 -0300

    se agrego un component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from "./components/header.component";
 
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
